c456d5f8ba43cf533b3fecd3774d7ccb7c1d0cd0
Weihong.Guan <weihong.Guan@gmail.com>
2013-02-22 00:32:28 +0800

    including 3 main types of animation in main()

--- greenrain.cpp
+++ greenrain.cpp
@@ -20,10 +20,19 @@ int main(int argc, char ** argv)
 
 	init_pair(1, COLOR_GREEN, 0);
 
-	for (int i=0; i<3; i++) {
-		flows.push_back(new DropFlowV(stdscr, 132 + i, rand() % 8));
-		flows.push_back(new DropFlow(stdscr, 32 + i, rand() % 4));
-	}
+	// clossing
+//	for (int i=0; i<3; i++) {
+//		flows.push_back(new DropFlowV(stdscr, 132 + i, rand() % 8));
+//		flows.push_back(new DropFlow(stdscr, 32 + i, rand() % 4));
+//	}
+
+	// horizental flow
+//	for (int i=0; i<LINES; i++) 
+//		flows.push_back(new DropFlow(stdscr, i, rand() % 8, rand() % COLS));
+
+	// vertical flow
+	for (int i=0; i<COLS; i++) 
+		flows.push_back(new DropFlowV(stdscr, i, rand() % 8, rand() % LINES));
 
 	while(getch() != 'q') {
 		for (std::vector<DropFlow *>::iterator it = flows.begin() ; it != flows.end(); ++it)
